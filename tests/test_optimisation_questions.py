91a87d375b1fa4c1c655e62c4be078aa8463addf
richelbilderbeek <richel@richelbilderbeek.nl>
2023-05-28 16:39:48 +0200

    Fix

--- tests/test_optimisation_questions.py
+++ tests/test_optimisation_questions.py
@@ -1,21 +1,6 @@
 """Tests all function in src.pfmp_richelbilderbeek.optimisation_questions."""
 import unittest
 
-from src.pfoptimisation_richelbilderbeek.optimisation_questions import (
-    is_zero,
-)
-
-
 class TestOptimisationQuestions(unittest.TestCase):
 
     """Class to test the functions in optimisation_questions.py."""
-
-    def test_is_zero(self):
-        """Test 'is_zero'."""
-        self.assertIsNotNone(is_zero.__doc__)
-        self.assertTrue(is_zero(0))
-        self.assertTrue(is_zero(0.0))
-        self.assertFalse(is_zero(1))
-        self.assertRaises(TypeError, is_zero, {1, 2})
-        self.assertRaises(TypeError, is_zero, "I am a string")
-
