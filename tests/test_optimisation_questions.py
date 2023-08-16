971dc27c9af1ba92968957877345a8be4a9da52c
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-16 16:38:19 +0200

    Fix style

--- tests/test_optimisation_questions.py
+++ tests/test_optimisation_questions.py
@@ -4,3 +4,7 @@ import unittest
 class TestOptimisationQuestions(unittest.TestCase):
 
     """Class to test the functions in optimisation_questions.py."""
+
+    def test_basics(self):
+        """Test some things that are true."""
+        self.assertTrue(1 + 1 == 2)
